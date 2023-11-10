5d5e00df486d2ee000cbaf521ad08b4404dd4e6b
Ritik Sadhou <114898377+RitikSadhou@users.noreply.github.com>
2023-11-10 08:08:05 +0000

    datatype

---
+++ Basic/Datatypeconversional.js
@@ -0,0 +1,27 @@
+//Datatype Conversional//
+
+let num="33wjfb";
+console.log(typeof num);
+console.log(typeof (num));
+
+let valueinnumber=Number(num);
+console.log(typeof valueinnumber);
+console.log( valueinnumber);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
