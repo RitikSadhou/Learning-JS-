39d142c455bd01962873ea31ceff5c7f9669c4fc
Ritik Sadhou <114898377+RitikSadhou@users.noreply.github.com>
2023-12-18 13:21:58 +0000

    Map chaning  Done

--- iterations/map.js
+++ iterations/map.js
@@ -1,5 +1,6 @@
 const myarr=[1,8,1,3,8,5,6,366]
 const newarr=myarr.map((item)=>item*10)
 //console.log(myarr);
-const chaining
-console.log(newarr);
+//console.log(newarr);
+const chaining=myarr.map((item)=>item*10).map((item)=>item-1).filter((item)=>item%3==0)
+console.log(chaining);
